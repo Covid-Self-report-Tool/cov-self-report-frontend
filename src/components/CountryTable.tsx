fad661290270fb3407e3eaf41406e3398db737e8
Jason Lampel <jason@bettermap.com>
2020-04-06 16:13:55 -0600

    Table/list: wrap in max-width Container (not done to other non-map views yet)

--- src/components/CountryTable.tsx
+++ src/components/CountryTable.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import {
+  Typography,
+  Container,
   Table,
   TableContainer,
   TableHead,
@@ -8,6 +11,12 @@ import {
   TableBody,
   Paper,
 } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(12),
+  },
+}));
 
 type CountryRow = {
   name: string;
@@ -26,40 +35,45 @@ type CountryTableType = {
 };
 
 export const CountryTable: FC<CountryTableType> = ({ data }) => {
+  const classes = useStyles();
+
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Country</TableCell>
-            <TableCell align="right">Confirmed</TableCell>
-            <TableCell align="right">Confirmed (Day Change)</TableCell>
-            <TableCell align="right">Dead</TableCell>
-            <TableCell align="right">Dead (Day Change)</TableCell>
-            <TableCell align="right">Recovered</TableCell>
-            <TableCell align="right">Recovered (Day Change)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(country => (
-            <TableRow key={country.name}>
-              <TableCell component="th" scope="row">
-                {country.name}
-              </TableCell>
-              <TableCell align="right">{country.confirmed}</TableCell>
-              <TableCell align="right">
-                {country.confirmed_day_change}
-              </TableCell>
-              <TableCell align="right">{country.dead}</TableCell>
-              <TableCell align="right">{country.dead_day_change}</TableCell>
-              <TableCell align="right">{country.recovered}</TableCell>
-              <TableCell align="right">
-                {country.recovered_day_change}
-              </TableCell>
+    <Container maxWidth="md" className={classes.root}>
+      <Typography variant="h2">Results by Country</Typography>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Country</TableCell>
+              <TableCell align="right">Confirmed</TableCell>
+              <TableCell align="right">Confirmed (Day Change)</TableCell>
+              <TableCell align="right">Dead</TableCell>
+              <TableCell align="right">Dead (Day Change)</TableCell>
+              <TableCell align="right">Recovered</TableCell>
+              <TableCell align="right">Recovered (Day Change)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.map(country => (
+              <TableRow key={country.name}>
+                <TableCell component="th" scope="row">
+                  {country.name}
+                </TableCell>
+                <TableCell align="right">{country.confirmed}</TableCell>
+                <TableCell align="right">
+                  {country.confirmed_day_change}
+                </TableCell>
+                <TableCell align="right">{country.dead}</TableCell>
+                <TableCell align="right">{country.dead_day_change}</TableCell>
+                <TableCell align="right">{country.recovered}</TableCell>
+                <TableCell align="right">
+                  {country.recovered_day_change}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
