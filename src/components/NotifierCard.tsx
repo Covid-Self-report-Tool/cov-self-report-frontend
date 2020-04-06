a430816f63a724250da5c750fe1b7ac632bfafd6
Jason Lampel <jason@abettermap.com>
2020-04-06 16:35:05 -0600

    Tickers: improve style, add more info and definition menu

--- src/components/NotifierCard.tsx
+++ src/components/NotifierCard.tsx
@@ -1,5 +1,17 @@
 import React, { FC } from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import clsx from 'clsx';
+import { Link as RouteLink } from 'react-router-dom';
+import {
+  ListSubheader,
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  Popover,
+  IconButton,
+} from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { prettyPrint } from 'utils';
@@ -7,15 +19,28 @@ import Title from './Title';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    textAlign: 'center',
+  },
+  paper: {
     display: 'flex',
     flexDirection: 'column',
     marginBottom: theme.spacing(2),
     overflow: 'auto',
-    padding: theme.spacing(2),
-    width: 200,
+    padding: theme.spacing(1),
+    position: 'relative',
+    width: 225,
   },
   cardSubText: {
     flex: 1,
+  },
+  popover: {
+    width: 250,
+  },
+  definitionText: {
+    whiteSpace: 'pre-wrap',
+  },
+  tickerVal: {
+    fontWeight: 'bold',
   },
 }));
 
@@ -24,18 +49,83 @@ type NotifierCard = {
   number: number;
 };
 
+// A popover menu and toggle button to show a term and its definition. For now
+// just links to "/about" route. May want sub-routes or at least "#section-id"
+// for scrolling directly to sections.
+function DefPopoverMenu({
+  defText = 'Definition goes here. Should be short and straightforward.',
+  defTerm = 'Def term',
+}: {
+  defText?: string;
+  defTerm?: string;
+}) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const definitionClassname = clsx(useStyles().definitionText);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-controls="ticker-info-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        size="small"
+      >
+        <InfoIcon color="primary" fontSize="inherit" />
+      </IconButton>
+      <Popover
+        id="ticker-info-menu"
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        classes={{ paper: useStyles().popover }}
+      >
+        <List
+          subheader={<ListSubheader>{defTerm}</ListSubheader>}
+          component="ul"
+          className={useStyles().popover}
+        >
+          <ListItem className={definitionClassname} divider>
+            {defText}
+          </ListItem>
+          <ListItem component={RouteLink} to="/about" onClick={handleClose}>
+            View info
+          </ListItem>
+        </List>
+      </Popover>
+    </div>
+  );
+}
+
 export const NotifierCard: FC<NotifierCard> = props => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
-      <Title>{props.text}</Title>
-      <Typography component="p" variant="h4">
-        {prettyPrint(props.number)}
-      </Typography>
-      <Typography color="textSecondary" className={classes.cardSubText}>
-        date/time here? and/or src link? or just in About?
-      </Typography>
-    </Paper>
+    <Grid item xs={12} className={classes.root}>
+      <Paper className={classes.paper}>
+        <Title>
+          {props.text}
+          <DefPopoverMenu />
+        </Title>
+        <Typography component="p" variant="h4" className={classes.tickerVal}>
+          {prettyPrint(props.number)}
+        </Typography>
+        <Typography
+          variant="caption"
+          color="textSecondary"
+          className={classes.cardSubText}
+        >
+          date/time if possible
+        </Typography>
+      </Paper>
+    </Grid>
   );
 };
