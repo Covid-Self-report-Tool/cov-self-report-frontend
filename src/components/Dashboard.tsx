8aa0aced1f8082888e4b74a351799c58fb036012
Jason Lampel <jason@bettermap.com>
2020-04-07 12:09:32 -0600

    Theme: switch to dark, use Bebas Neue everywhere except About (Roboto)

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,30 @@
 import React, { FC } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import { useStyles } from './dashboard.styles';
 import { MainNavBar, ListItems } from 'components';
+
+let theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+  typography: {
+    fontFamily: [
+      'Bebas Neue',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    fontSize: 18,
+  },
+});
+theme = responsiveFontSizes(theme);
 
 type DashboardTypes = {
   children: React.ReactNode | null | undefined;
@@ -13,13 +35,15 @@ export const Dashboard: FC<DashboardTypes> = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <MainNavBar />
-      <main className={classes.content}>
-        {children}
-        <ListItems />
-      </main>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <MainNavBar />
+        <main className={classes.content}>
+          {children}
+          <ListItems />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 };
