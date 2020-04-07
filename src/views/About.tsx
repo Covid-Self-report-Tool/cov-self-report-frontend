c4cc690311922c62865d8a5c05733a4d987da77f
Jason Lampel <jason@abettermap.com>
2020-04-07 12:17:58 -0600

    About: add placeholder and preliminary text from Guneeta's doc

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -32,6 +32,84 @@ export const About: FC = () => {
   return (
     <Container className={allStyles}>
       <h1>About this project</h1>
+      <p>
+        XXX is a COVID-19 self-reporting tool that aggregates crowdsourced data
+        to aid in predictive modeling of the viral spread. Predictive modeling
+        will help us strategize solutions to curb the spread of and potentially
+        support eradication of COVID-19. By adding your information to this site
+        you can supplement the information available from the CDC, WHO, and ECDC
+        you can add to the data available that help scientists and medical
+        workers fight this disease. Your contributions will be anonymous?
+      </p>
+      <h2>Motivation</h2>
+      <p>
+        As scientists we understand that data critical for developing a true and
+        realistic understanding of COVID-19. Moved by the grave pandemic facing
+        all of us, we felt it was our duty as individuals with advanced training
+        in the sciences to come together and create a tool that fills the gaps
+        in our knowledge for COVID-19.
+      </p>
+      <p>
+        One of the major problems facing our global community at this time is
+        the lack of testing. Presently, most countries only offer testing to
+        individuals in critical conditions or those who are hospitalized.
+        Because of this, many individuals aren’t being tested for COVID-19 and
+        official global data on the number of people infected with the virus,
+        and the viral spread are inaccurate and misleading.
+      </p>
+      <p>
+        Motivated by this problem we have come together to combine our skills
+        and create a tool that will create more transparency on the spread of
+        COVID-19 and provide the data we need to overcome this pandemic as a
+        global community.
+      </p>
+      <h2>Data resources</h2>
+      <p>WHO/CDC/ECDC/ other...</p>
+      <h2>Privacy Policy</h2>
+      <p>(important as we are asking self reporting) - will get</p>
+      <h2>Terms and Conditions</h2>{' '}
+      <p>
+        I communicated with a law firm willing to volunteer (McDermott Wills and
+        Emory). They help us at the archive and will see if they can help an
+        unincorporated group of individuals (Guneeta)
+      </p>
+      <h2>Team Information</h2>
+      <ul>
+        <li>
+          <b>Nathan Heston, PhD</b> Teaches physics at California Polytechnic
+          State University and does research in mathematical modeling of solid
+          state materials.
+        </li>
+        <li>
+          <b>Nick Wolfe</b>
+        </li>
+        <li>
+          <b>Sumit Arora</b>
+        </li>
+        <li>
+          <b>Guneeta Singh Bhalla</b> has a PhD in physics from the University
+          of Florida and completed her post doctoral research at the University
+          of California at Berkeley and the Lawrence Berkeley National
+          Laboratory in the field of Condensed Matter Physics.
+        </li>
+        <li>
+          <b>Gabriel Abud</b>
+        </li>
+        <li>
+          <b>Jason Lampel</b> owns and develops at{' '}
+          <a href="https://www.abettermap.com" target="_blank">
+            A Better Map
+          </a>
+          .
+        </li>
+      </ul>
+      <h2>Self reporting form</h2>
+      <h3>Questionnaire</h3>
+      <p>see relevant document</p>
+      <h2>Data Models</h2>
+      <p>Nathan and Nick are working on this</p>
+      <h2>World Map and Data Visualization</h2>
+      <p>interactive (clickable) igeo-temporal maps would be great</p>
     </Container>
   );
 };
