b33164ae1ce572de0456fe99c245c8ec2cbb36bd
Gabe Abud <gabriel.jabud@gmail.com>
2020-04-11 19:49:12 -0700

    adds title

--- src/components/submission/steps/SymptomDatesStep.tsx
+++ src/components/submission/steps/SymptomDatesStep.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { FC } from 'react';
+import {
+  DialogTitle,
+  DialogContent,
+  FormGroup,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+} from '@material-ui/core';
+import { SymptomForm, Symptoms, DispatchFormType } from 'types/submission';
 
-export const SymptomDatesStep = () => {
-  return <></>;
+type SymptomDatesStepType = {
+  formState: SymptomForm;
+  dispatchForm: DispatchFormType;
+};
+
+export const SymptomDatesStepType: FC<SymptomDatesStepType> = ({
+  formState,
+  dispatchForm,
+}) => {
+  return (
+    <>
+      <DialogTitle>When did you feel these?</DialogTitle>
+      <DialogContent></DialogContent>
+    </>
+  );
 };
