adfb7e83fcaad403304a98486644284a0bd56cac
Jason Lampel <jason@abettermap.com>
2020-04-15 10:16:54 -0600

    Symptoms modal success confirmation: use legit text, improve style

--- src/components/SuccessConfModal.tsx
+++ src/components/SuccessConfModal.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Slide,
+  Typography,
+} from '@material-ui/core';
 import { TransitionProps } from '@material-ui/core/transitions';
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement<any, any> },
@@ -20,12 +25,21 @@ interface SuccessConfModalTypes {
   setOpen: React.Dispatch<boolean>;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    icon: {
+      color: theme.palette.success.main,
+    },
+    iconWrap: {
+      marginBottom: 0,
+      lineHeight: 0,
+    },
+  })
+);
+
 export const SuccessConfModal: FC<SuccessConfModalTypes> = props => {
   const { open, setOpen } = props;
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  const classes = useStyles();
 
   const handleClose = () => {
     setOpen(false);
@@ -34,6 +48,7 @@ export const SuccessConfModal: FC<SuccessConfModalTypes> = props => {
   return (
     <Dialog
       open={open}
+      // open={true}
       TransitionComponent={Transition}
       keepMounted
       onClose={handleClose}
@@ -41,20 +56,36 @@ export const SuccessConfModal: FC<SuccessConfModalTypes> = props => {
       aria-describedby="alert-dialog-slide-description"
     >
       <DialogTitle id="alert-dialog-slide-title">
-        {"Use Google's location service?"}
+        Submission sent successfully
       </DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-slide-description">
-          Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.
+      <DialogContent dividers>
+        <Typography variant="h2" align="center" className={classes.iconWrap}>
+          <CheckCircleRoundedIcon className={classes.icon} fontSize="inherit" />
+        </Typography>
+        <DialogContentText
+          id="alert-dialog-slide-description"
+          className={`simpler-font`}
+          variant="body2"
+        >
+          <p>
+            Thank you for adding your case to the world map. You're helping us
+            understand COVID-19 so that we can fight it together. Please share
+            this with friends who are unwell. Send us your thoughts and
+            feedback. Sign up for email updates on the project.
+          </p>
+          <p>
+            To get support and learn more about COVID-19, please visit the World
+            Health Organization’s COVID-19 page.
+          </p>
+          <p>
+            COMING SOON: Get updates on current infections in your area and how
+            they are changing.
+          </p>
         </DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
-          Disagree
-        </Button>
-        <Button onClick={handleClose} color="primary">
-          Agree
+          Close
         </Button>
       </DialogActions>
     </Dialog>
