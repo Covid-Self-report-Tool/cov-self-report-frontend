17ee7d375eda66474c7d464f90eb530945608bbe
Jason Lampel <jason@bettermap.com>
2020-04-17 20:27:54 -0600

    Tickers: bring in real text, improve style, omit date for self-reported:
    Also move Popover stuff into separate component file.

---
+++ src/components/TickerInfoPopover.tsx
@@ -0,0 +1,103 @@
+import React, { FC, useContext } from 'react';
+import { Link as RouteLink } from 'react-router-dom';
+import {
+  ListSubheader,
+  Typography,
+  List,
+  ListItem,
+  Popover,
+  IconButton,
+} from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { TickerInfoType } from 'types';
+import { GlobalContext } from 'components';
+
+const useStyles = makeStyles(theme => ({
+  popover: {
+    width: 250,
+  },
+  definitionText: {
+    whiteSpace: 'pre-wrap',
+  },
+  infoBtn: {
+    padding: 0,
+    position: 'absolute',
+    top: 3,
+    right: 3,
+    fontSize: '1rem',
+  },
+  viewInfoLink: {
+    color: theme.palette.info.main,
+    '& :visited': {
+      color: theme.palette.info.main,
+    },
+  },
+}));
+
+// A popover menu and toggle button to show a term and its definition. For now
+// just links to "/about" route. May want sub-routes or at least "#section-id"
+// for scrolling directly to sections.
+export const TickerInfoPopover: FC<TickerInfoType> = ({
+  defText,
+  heading,
+  omitLastUpdated,
+}) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const classes = useStyles();
+  const { state } = useContext(GlobalContext);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButton
+        aria-controls="ticker-info-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        size="small"
+        className={classes.infoBtn}
+      >
+        <InfoIcon color="primary" fontSize="inherit" />
+      </IconButton>
+      <Popover
+        id="ticker-info-menu"
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        classes={{ paper: classes.popover }}
+      >
+        <List
+          subheader={<ListSubheader>{heading}</ListSubheader>}
+          component="ul"
+          className={classes.popover}
+        >
+          <ListItem className={classes.definitionText}>{defText}</ListItem>
+          <ListItem divider>
+            {!omitLastUpdated && (
+              <Typography variant="caption" color="textSecondary">
+                LAST UPDATED: {state.lastCountriesUpdate}
+              </Typography>
+            )}
+          </ListItem>
+          <ListItem
+            component={RouteLink}
+            to="/about"
+            onClick={handleClose}
+            className={classes.viewInfoLink}
+          >
+            View info
+          </ListItem>
+        </List>
+      </Popover>
+    </>
+  );
+};
