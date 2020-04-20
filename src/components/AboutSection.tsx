f8589b7dbe32fc09ada6dd9b050df1fa0d14fe39
Jason Lampel <jason@bettermap.com>
2020-04-20 17:59:50 -0600

    About: show error in UI if file fetch fails

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect, useState } from 'react';
 import { CircularProgress } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
 import { getAboutPage } from 'utils/api';
 
@@ -9,21 +10,27 @@ interface AboutType {
 
 export const AboutSection: FC<AboutType> = ({ filename }) => {
   const [html, setHtml] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     getAboutPage(filename)
       .then(response => {
         setHtml(response.text);
       })
-      .catch(err => console.error(err)); // TODO: handle this better
+      .catch(err => {
+        setError(true);
+        console.error(err);
+      });
   }, []);
 
   return (
     <div>
-      {html ? (
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      ) : (
-        <CircularProgress size={30} />
+      {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
+      {!html && !error && <CircularProgress size={30} />}
+      {error && (
+        <Alert severity="error" variant="filled">
+          Could not get {filename}
+        </Alert>
       )}
     </div>
   );
