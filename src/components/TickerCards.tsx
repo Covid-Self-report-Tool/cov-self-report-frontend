1ffb595d810c94849b51c7e817eed7fa8da0b851
Jason Lampel <jason@bettermap.com>
2020-04-21 15:28:04 -0600

    Ticker cards: move card heading into parent component file:
    ...since it's not used anywhere else and wouldn't make sense to.

--- src/components/TickerCards.tsx
+++ src/components/TickerCards.tsx
@@ -7,8 +7,6 @@ import { CurrentTotalsTypes } from 'context/types';
 import { prettyPrint } from 'utils';
 import { TickerInfoPopover } from 'components';
 
-import Title from './Title';
-
 const useStyles = makeStyles(theme => ({
   root: {
     textAlign: 'center',
@@ -26,6 +24,10 @@ const useStyles = makeStyles(theme => ({
       marginBottom: theme.spacing(1),
       width: 135,
     },
+  },
+  heading: {
+    lineHeight: 1,
+    fontSize: '1rem',
   },
   tickerVal: {
     color: theme.palette.common.black,
@@ -58,6 +60,25 @@ interface TickerCard extends TickerInfoType {
   omitLastUpdated?: boolean;
 }
 
+interface CardTitleTypes {
+  heading: string;
+}
+
+const CardTitle: FC<CardTitleTypes> = ({ heading }) => {
+  const classes = useStyles();
+
+  return (
+    <Typography
+      component="h2"
+      variant="subtitle2"
+      color="primary"
+      className={classes.heading}
+    >
+      {heading}
+    </Typography>
+  );
+};
+
 const NotifierCard: FC<TickerCard> = props => {
   const classes = useStyles();
   const { heading, number } = props;
@@ -65,7 +86,7 @@ const NotifierCard: FC<TickerCard> = props => {
   return (
     <Grid item className={classes.root}>
       <Paper className={classes.paper}>
-        <Title>{heading}</Title>
+        <CardTitle heading={heading} />
         <Typography component="p" variant="h4" className={classes.tickerVal}>
           {number ? prettyPrint(number) : <CircularProgress size={30} />}
         </Typography>
