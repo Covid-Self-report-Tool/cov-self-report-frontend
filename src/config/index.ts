9d2581f5be6d9fb42d0f9052b167c7bb253044a4
Jason Lampel <jason@abettermap.com>
2020-04-23 11:10:57 -0600

    About HTML content: from GitHub instead of S3

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const MAPBOX_API_KEY =
   'pk.eyJ1IjoiYWJvb2QxMDE2IiwiYSI6ImNrOG5mZWwyODB4eXYzbHBoaXoyMzVpMTIifQ.-NSMo7xsq7Rmq0t0JyizGg';
-export const AWS_S3_BASE_URL =
-  'https://self-tracker-tool-public.s3.amazonaws.com/static-web-content';
+export const CLOUD_HTML_BASE_URL =
+  'https://raw.githubusercontent.com/abettermap/c19-self-report-content/master';
 
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
