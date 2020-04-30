7a6bdf5f38803176eb1c482dc5bf3b3a5e458612
Gabe Abud <gabriel.jabud@gmail.com>
2020-04-30 08:03:48 -0700

    changes title to "not scare people away"

--- src/components/submission/steps/RegistrationStep.tsx
+++ src/components/submission/steps/RegistrationStep.tsx
@@ -43,7 +43,7 @@ export const RegistrationStep: FC<RegistrationStepType> = ({
 
   return (
     <>
-      <DialogTitle>Register</DialogTitle>
+      <DialogTitle>Submission</DialogTitle>
       <DialogContent>
         <IfFirebaseUnAuthed>
           {() => (
