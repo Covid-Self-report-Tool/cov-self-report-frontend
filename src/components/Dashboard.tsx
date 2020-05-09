905f2621c9eb95052145a17783b1aacab9db70ee
Jason Lampel <jason@bettermap.com>
2020-05-08 21:30:05 -0600

    Show "unsupported browser" message in symptoms modal instead of splash

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,6 @@ import { Container, Typography } from '@material-ui/core';
 import { SiteNavBtns, BackToTopBtn, MobileOffCanvasNav } from 'components';
 import { TopBar } from 'components/top-bar';
 import { useStyles } from './dashboard.styles';
-import { isValidUserAgent } from '../utils';
 
 type DashboardTypes = {
   children: React.ReactNode | null | undefined;
@@ -17,47 +16,28 @@ export const Dashboard: FC<DashboardTypes> = ({ children }) => {
   const isHome = useLocation().pathname === '/';
   const [drawerOpen, toggleDrawerOpen] = useState<boolean>(false);
 
-  if (isValidUserAgent()) {
-    return (
-      <div className={classes.root}>
-        <TopBar
-          isHome={isHome}
-          drawerOpen={drawerOpen}
-          toggleDrawerOpen={toggleDrawerOpen}
-        />
-        <div id="back-to-top-anchor" />
-        {isHome ? (
-          children
-        ) : (
-          <>
-            <Container component="main" className={classes.content}>
-              {children}
-              <BackToTopBtn />
-            </Container>
-          </>
-        )}
-        <MobileOffCanvasNav
-          drawerOpen={drawerOpen}
-          toggleDrawerOpen={toggleDrawerOpen}
-        >
-          <SiteNavBtns />
-        </MobileOffCanvasNav>
-      </div>
-    );
-  }
   return (
-    <div className={classes.browserMessage}>
-      <Typography align="center" gutterBottom variant="h4">
-        COVID Self-Reporting Tool
-      </Typography>
-      <Typography align="center" gutterBottom variant="body1">
-        Please open the COVID Self-Reporting Tool in one of our supported
-        browsers.
-      </Typography>
-      <Typography align="center" gutterBottom variant="body2">
-        Supported Browsers: Google Chrome, Safari, Mozilla Firefox, Internet
-        Explorer
-      </Typography>
+    <div className={classes.root}>
+      <TopBar
+        isHome={isHome}
+        drawerOpen={drawerOpen}
+        toggleDrawerOpen={toggleDrawerOpen}
+      />
+      <div id="back-to-top-anchor" />
+      {isHome ? (
+        children
+      ) : (
+        <Container component="main" className={classes.content}>
+          {children}
+          <BackToTopBtn />
+        </Container>
+      )}
+      <MobileOffCanvasNav
+        drawerOpen={drawerOpen}
+        toggleDrawerOpen={toggleDrawerOpen}
+      >
+        <SiteNavBtns />
+      </MobileOffCanvasNav>
     </div>
   );
 };
