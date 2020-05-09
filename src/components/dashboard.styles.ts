9afdb22404f97705d6c5b56c70abe0ab05f21b40
Jot Samra <amanjotksamra@gmail.com>
2020-05-08 19:05:16 -0700

    fixing spacing issues

--- src/components/dashboard.styles.ts
+++ src/components/dashboard.styles.ts
@@ -53,6 +53,6 @@ export const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(8),
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 }));
