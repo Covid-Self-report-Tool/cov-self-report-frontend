2150ed8e041fa806a105e72e358693657e37d9b3
Jason Lampel <jason@abettermap.com>
2020-05-23 11:52:55 -0600

    Submission success modal: improve style/layout, beautify code, flatten/simplify markup

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react';
 
 export const Disclaimer: FC = () => {
   return (
-    <p>
+    <p className="simpler-font" style={{ fontSize: 11 }}>
       <b>Disclaimer:&nbsp;</b>
-      This tool will not provide a diagnosis, nor will it
-      count you as a confirmed coronavirus case based on symptoms alone.
-      By providing this information you will help build a better understanding
-      of how widespread COVID-19 may be in your area, and ultimately help
-      communities overcome this crisis together. Your privacy and security come
-      first, and only you control your data.
+      This tool will not provide a diagnosis, nor will it count you as a
+      confirmed coronavirus case based on symptoms alone. By providing this
+      information you will help build a better understanding of how widespread
+      COVID-19 may be in your area, and ultimately help communities overcome
+      this crisis together. Your privacy and security come first, and only you
+      control your data.
     </p>
-  )
-}
+  );
+};
--- src/components/SuccessConfModal.tsx
+++ src/components/SuccessConfModal.tsx
@@ -1,21 +1,22 @@
 import React, { FC, useContext } from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
+  Divider,
   Slide,
   Typography,
+  useMediaQuery,
 } from '@material-ui/core';
 import { TransitionProps } from '@material-ui/core/transitions';
 import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
+
 import { GlobalContext, ShareButtons } from 'components';
 import { Disclaimer } from './Disclaimer';
 import { useSubmitted } from '../utils/queries';
-
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement<any, any> },
@@ -29,32 +30,34 @@ interface SuccessConfModalTypes {
   setOpen: React.Dispatch<boolean>;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    icon: {
-      color: theme.palette.success.main,
+const useStyles = makeStyles((theme: Theme) => ({
+  icon: {
+    color: theme.palette.success.main,
+    marginRight: 4,
+  },
+  dialogContent: {
+    '& a': {
+      color: theme.palette.info.main,
     },
-    iconWrap: {
-      marginBottom: 0,
-      lineHeight: 0,
+  },
+  dialog: {
+    '& .MuiDialogTitle-root > .MuiTypography-root': {
+      display: 'flex',
+      alignItems: 'center',
     },
-    dialogContent: {
-      '& a': {
-        color: theme.palette.info.main,
-      },
-    },
-    disclaimer: {
-      fontSize: 13,
-    },
-  }),
-);
+  },
+  divider: {
+    margin: theme.spacing(2),
+  },
+}));
 
 export const SuccessConfModal: FC<SuccessConfModalTypes> = props => {
-  const { open, setOpen } = props;
   const classes = useStyles();
-
-  const { state, dispatch } = useContext(GlobalContext);
+  const theme = useTheme();
+  const { dispatch } = useContext(GlobalContext);
+  const { open, setOpen } = props;
   const { data: submissions } = useSubmitted(dispatch);
+  const smallAndUp = useMediaQuery(theme.breakpoints.up('sm'));
 
   const handleClose = () => {
     setOpen(false);
@@ -63,75 +66,63 @@ export const SuccessConfModal: FC<SuccessConfModalTypes> = props => {
   return (
     <Dialog
       open={open}
+      fullScreen={!smallAndUp}
       data-cy="successful-submission"
       transitionDuration={{ enter: 900, exit: 400 }}
       TransitionComponent={Transition}
       onClose={handleClose}
       aria-labelledby="alert-dialog-slide-title"
       aria-describedby="alert-dialog-slide-description"
+      className={classes.dialog}
     >
       <DialogTitle id="alert-dialog-slide-title">
-        Submission sent successfully
+        <CheckCircleRoundedIcon className={classes.icon} />
+        Submission sent
       </DialogTitle>
       <DialogContent dividers className={classes.dialogContent}>
-        <Typography variant="h2" align="center" className={classes.iconWrap}>
-          <CheckCircleRoundedIcon className={classes.icon} fontSize="inherit" />
+        <Typography align="center">
+          Thank you for contributing! So far,{' '}
+          {submissions && submissions.length} users like you have added
+          themselves to the world map.
         </Typography>
-        <DialogContentText
-          id="alert-dialog-slide-description"
-          className={`simpler-font`}
-          variant="body2"
-        >
-          <p>
-            Thank you for contributing! So far, {submissions && submissions.length} users like you have
-            added themselves to the world map.
-          </p>
-          <p>
-            <ShareButtons/>
-          </p>
-          <p>
-            <DialogContentText
-              id="alert-dialog-slide-description"
-              variant="body2"
+        <Divider variant="middle" className={classes.divider} />
+        <ShareButtons />
+        <Typography variant="h5" paragraph>
+          What you can do next:
+        </Typography>
+        <ol className="simpler-font" style={{ fontSize: 16, paddingLeft: 30 }}>
+          <li>
+            Share this with your friends and family by using the share buttons
+            above.
+          </li>
+          <li>
+            Provide your{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfkQJMihQUlA6scYvjr1A1OZiXGRRxQLkD1YIiklGDq5YTclQ/viewform?usp=sf_link"
             >
-              What you can do next:
-            </DialogContentText>
-            <ol>
-              <li>
-                Share this with your friends and family by using the share buttons above.
-              </li>
-              <li>
-                Share your{' '}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSfkQJMihQUlA6scYvjr1A1OZiXGRRxQLkD1YIiklGDq5YTclQ/viewform?usp=sf_link"
-                >
-                  thoughts and feedback
-                </a>
-                .
-              </li>
-              <li>
-                Learn more about your symptoms at the{' '}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
-                >
-                  World Health Organization’s COVID-19 page
-                </a>
-                .
-              </li>
-              <li>
-                Come back soon and get updates on trends of potential and confirmed
-                infections in your area.
-              </li>
-            </ol>
-          </p>
-          <div className={classes.disclaimer}>
-            <Disclaimer/>
-          </div>
-        </DialogContentText>
+              thoughts and feedback
+            </a>
+            .
+          </li>
+          <li>
+            Learn more about your symptoms at the{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+            >
+              World Health Organization’s COVID-19 page
+            </a>
+            .
+          </li>
+          <li>
+            Come back soon and get updates on trends of potential and confirmed
+            infections in your area.
+          </li>
+        </ol>
+        <Disclaimer />
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
