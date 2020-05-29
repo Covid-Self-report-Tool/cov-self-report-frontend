65d86f3020e540391dfde63f549a6ef673e52563
abettermap <jason@abettermap.com>
2020-05-29 23:19:06 +0000

    Deploying to gh-pages from  @ 6982896e01190bf1f4147a79aea2c490c9ebc839 🚀

---
+++ review/jason/gh-actions-init/precache-manifest.c35042692a78705d18a0c6759db257ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f52082f53c9ac4b829c555b8213f03b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30a0d409ae98baf1d05b",
+    "url": "./static/css/2.fb66d78c.chunk.css"
+  },
+  {
+    "revision": "30a0d409ae98baf1d05b",
+    "url": "./static/js/2.9cb9f066.chunk.js"
+  },
+  {
+    "revision": "087d8715dbe9fe288ecd174c8b7613c0",
+    "url": "./static/js/2.9cb9f066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c605ed5d8de6a21d4009",
+    "url": "./static/js/main.bf36b674.chunk.js"
+  },
+  {
+    "revision": "c350ff337de434631df0",
+    "url": "./static/js/runtime-main.e1b0ad46.js"
+  }
+]);
--- review/jason/gh-actions-init/service-worker.js
+++ review/jason/gh-actions-init/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/refs/heads/jason/gh-actions-init/precache-manifest.85734400dcb4b5c12bc80950423d1570.js"
+  "./precache-manifest.c35042692a78705d18a0c6759db257ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/refs/heads/jason/gh-actions-init/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
