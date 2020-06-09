82c7a342f993f84ecfc0eca11776973d6d754202
Jason Lampel <jason@abettermap.com>
2020-06-09 11:57:43 -0600

    Map: add legally required Mapbox wordmark

--- src/components/map/index.ts
+++ src/components/map/index.ts
@@ -1,5 +1,6 @@
 export * from './Map';
 export * from './MapboxTileLayer';
+export * from './MapboxWordmark';
 export * from './Polygons';
 export * from './PolygonTooltip';
 export * from './SubmittedCases';
